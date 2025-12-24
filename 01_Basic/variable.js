fedad37a3b00d16e297bf66760d8c274427a7dd8
RaviVerma-ghrepo <vermaraviranjan242@gmail.com>
2025-12-24 10:52:25 +0000

    Variable Type Done

---
+++ 01_Basic/variable.js
@@ -0,0 +1,12 @@
+const classId = 12345
+let className = "RaviVerma"
+var classMail = "ravi@google.com"
+classNumber = 12
+
+// classId = 123, It cannot be changed due to const type of variables.
+
+// let type variable has always been changed.
+className = "ranjan"
+console.log(className)
+
+console.table([classId, className, classMail, classNumber])
